d6635f4a86a54582170b4268751df7d40fddfd92
Juan Morales <juan1810.jm@gmail.com>
2023-03-30 21:59:36 -0300

    correccion de bugs

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,27 +21,32 @@ formulario.addEventListener("submit", (evento) => {
 
   const botonCalculcular = formulario.querySelector("[data-calcular]");
   botonCalculcular.addEventListener("click", () => {
-    if (contador == 0){
-      alert('ingrese nombre y monto antes de calcular');
+    if (contador != 0){
+      document.getElementById('calcular').style.display = 'none';
+      document.getElementById('nuevoCalculo').style.display = 'block';
+      var preciosConNombres = totalPrecios.map((precio, indice) => {
+        return { precio, nombre: nombres[indice] };
+      });
+      preciosConNombres.sort((a, b) => b.precio - a.precio);
+      totalPrecios = preciosConNombres.map(obj => obj.precio);
+      nombres = preciosConNombres.map(obj => obj.nombre);
+      let countnombres = 0
+      nombres.forEach(function(nombres) {
+          let divicion = totalPrecios[countnombres]/contador;
+          if(divicion!=0){
+            const result = document.querySelector("[data-result]");
+            const nuevaLinea = div.card2(nombres, divicion);
+            result.appendChild(nuevaLinea);
+          }
+        countnombres++
+      });
     }else
-    var preciosConNombres = totalPrecios.map((precio, indice) => {
-      return { precio, nombre: nombres[indice] };
-    });
-    preciosConNombres.sort((a, b) => b.precio - a.precio);
-    totalPrecios = preciosConNombres.map(obj => obj.precio);
-    nombres = preciosConNombres.map(obj => obj.nombre);
-    let countnombres = 0
-    nombres.forEach(function(nombres) {
-        let divicion = totalPrecios[countnombres]/contador;
-        if(divicion!=0){
-          const result = document.querySelector("[data-result]");
-          const nuevaLinea = div.card2(nombres, divicion);
-          result.appendChild(nuevaLinea);
-        }
-      countnombres++
-    });
+    alert('ingrese nombre y monto antes de calcular');
   });
-
+const botonNuevoCalculo = formulario.querySelector("[data-nuevocalc]");
+botonNuevoCalculo.addEventListener("click", () => {
+  location.reload();
+});
   export let count = {
     contador
   }
